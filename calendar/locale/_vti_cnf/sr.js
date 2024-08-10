6a7cb92429ebbfcba17d12bbe6a7e9141b1a83ce
Ashraful Islam <ashrafuli.us@gmail.com>
2024-08-10 15:50:58 -0500

    1st commit

---
+++ calendar/locale/_vti_cnf/sr.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|26 Jul 2017 13:11:41 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|LAPTOP-4SNOKE3L\\Ashraful
+vti_modifiedby:SR|LAPTOP-4SNOKE3L\\Ashraful
+vti_timecreated:TR|26 Jul 2017 13:11:41 -0000
+vti_cacheddtm:TX|26 Jul 2017 13:11:41 -0000
+vti_filesize:IR|3048
+vti_backlinkinfo:VX|
